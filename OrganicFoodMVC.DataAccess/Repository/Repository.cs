d4d4f5d2fd446026b9c692f23a547a82f54583e2
trongthuong96 <dotrongthuong8@gmail.com>
2022-04-19 23:37:26 +0700

    Set repository

---
+++ OrganicFoodMVC.DataAccess/Repository/Repository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrganicFoodMVC.DataAccess.Data;
+using OrganicFoodMVC.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OrganicFoodMVC.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OrganicFoodMVC.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OrganicFoodMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Thể Loại")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Display(Name="Mô tả")]
+        [MaxLength(5000)]
+        public string Discription { get; set; }
+
+    }
+}
