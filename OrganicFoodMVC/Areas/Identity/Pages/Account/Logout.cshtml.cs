2df4164b6e06cedb5c0c201745663f30ba73b83c
trongthuong96 <dotrongthuong8@gmail.com>
2022-06-14 23:49:11 +0700

    fix search category menu

--- OrganicFoodMVC/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ OrganicFoodMVC/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -30,7 +30,9 @@ namespace OrganicFoodMVC.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
+            //
             HttpContext.Session.SetInt32(SD.ssShoppingCart, 0);
+
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
